a42db96a69d5a388581890aaaf81cc2953da2d76
Erik Romijn <eromijn@solidlinks.nl>
2016-03-06 17:27:12 +0100

    Removed integration tests. They're maintenance intensive and overkill for this project.

--- run_ci_tests.bash
+++ run_ci_tests.bash
@@ -7,14 +7,12 @@ ret=0
 
 case $CIRCLE_NODE_INDEX in
     0)
-    flake8 happinesspackets || ret=$?
-    ./manage.py makemigrations --dry-run --noinput | grep -i 'no changes detected' || ret=$?
-
     coverage run ./manage.py test -v 2 --noinput && coverage html --fail-under=90 || ret=$?
     ;;
 
     1)
-    ./manage.py test -v 2 -p integration_test*.py --settings=happinesspackets.settings.ci --noinput || ret=$?
+    flake8 happinesspackets || ret=$?
+    ./manage.py makemigrations --dry-run --noinput | grep -i 'no changes detected' || ret=$?
     ;;
 esac
 
