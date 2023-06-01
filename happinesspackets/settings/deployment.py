2593dfcea1d7fc166b67a97099da25cbe18eedb1
Sasha Romijn <github@mxsasha.eu>
2023-06-01 13:27:14 +0100

    bla

--- happinesspackets/settings/deployment.py
+++ happinesspackets/settings/deployment.py
@@ -29,13 +29,6 @@ TEMPLATES[0]['OPTIONS']['loaders'] = (
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
-
 LOGGING['handlers']['file'] = {
     'level': 'INFO',
     'class': 'logging.handlers.WatchedFileHandler',
